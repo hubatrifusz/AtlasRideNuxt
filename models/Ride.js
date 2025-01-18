84b72d69e9d3b137025e22fcc25029d19d76118c
hubatrifusz <huba.trifusz@gmail.com>
2025-01-18 23:01:45 +0100

    Schema updated

--- models/Ride.js
+++ models/Ride.js
@@ -1,7 +1,27 @@
 const mongoose = require("mongoose");
 
 const RideSchema = new mongoose.Schema({
-  name: {
+  lastname: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
     type: String,
     required: true,
   }
