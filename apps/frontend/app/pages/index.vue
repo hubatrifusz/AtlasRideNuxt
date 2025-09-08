9653775eb6469a0216863caf32c8e4b4e2a5258e
Huba Trifusz <huba.trifusz@gmail.com>
2025-09-08 18:56:41 +0200

    feat: update .gitignore, enhance main.css styles, modify index.vue structure, and add @nuxt/fonts dependency

--- apps/frontend/app/pages/index.vue
+++ apps/frontend/app/pages/index.vue
@@ -1,5 +1,9 @@
 <script lang="ts"></script>
 
-<template></template>
+<template>
+  <div>
+    <UButton color="primary" size="xl" trailing-icon="i-lucide-clipboard-pen" class="px-12 py-4 text-xl fixed bottom-16 left-1/2 transform -translate-x-1/2 cursor-pointer">Ajánlatkérés</UButton>
+  </div>
+</template>
 
 <style scoped></style>
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,9 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/ui', '@nuxt/fonts'],
+  css: ['~/assets/css/main.css'],
   vite: { plugins: [tailwindcss()] },
 });
