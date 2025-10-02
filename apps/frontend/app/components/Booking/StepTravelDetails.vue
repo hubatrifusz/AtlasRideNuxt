979433b129b70463857b4bd9b724126607b370f6
Huba Trifusz <huba.trifusz@gmail.com>
2025-10-02 19:36:14 +0200

    feat: add flight details inputs for airport transfers and update calendar locale to Hungarian

--- apps/frontend/app/components/Booking/StepTravelDetails.vue
+++ apps/frontend/app/components/Booking/StepTravelDetails.vue
@@ -81,39 +81,6 @@ function updateField<K extends keyof typeof props.form>(key: K, value: any) {
       @update:model-value="updateField('passengers', $event)"
     />
 
-    <div class="md:w-1/2">
-      <USwitch :model-value="props.form.return" label="Retúr" size="xl" @update:model-value="updateField('return', $event)" />
-    </div>
-
-    <!-- Retúr esetén -->
-    <div v-if="props.form.return" class="w-full flex flex-col items-center gap-4">
-      <UPopover
-        :content="{
-          align: 'center',
-          side: 'bottom',
-          sideOffset: 2,
-        }"
-      >
-        <UButton size="xl" icon="i-lucide-calendar" class="text-text-primary md:w-1/2 w-full" color="secondary">
-          {{ props.form.returnDate ? df.format(props.form.returnDate.toDate(getLocalTimeZone())) : 'Válassza ki a visszaút dátumát' }}
-        </UButton>
-        <template #content>
-          <UCalendar :model-value="props.form.returnDate" :year-controls="false" size="lg" :min-value="minDate" @update:model-value="updateField('returnDate', $event)" />
-        </template>
-      </UPopover>
-
-      <UInput
-        v-maska="'##:##'"
-        :model-value="props.form.returnTime"
-        trailing-icon="i-lucide-timer"
-        label="Visszaút indulási ideje"
-        placeholder="Visszaút indulási ideje"
-        size="xl"
-        class="md:w-1/2 w-full"
-        @update:model-value="updateField('returnTime', $event)"
-      />
-    </div>
-
     <!-- Repteri esetén -->
     <div v-if="props.form.rideType == 'reptéri'" class="w-full flex flex-col items-center gap-2">
       <USeparator class="md:w-1/2 w-full my-8" />
@@ -135,6 +102,46 @@ function updateField<K extends keyof typeof props.form>(key: K, value: any) {
         size="xl"
         class="md:w-1/2 w-full"
         @update:model-value="updateField('takeoffTime', $event)"
+      />
+    </div>
+
+    <div class="md:w-1/2">
+      <USwitch :model-value="props.form.return" label="Retúr" size="xl" @update:model-value="updateField('return', $event)" />
+    </div>
+
+    <!-- Retúr esetén -->
+    <div v-if="props.form.return" class="w-full flex flex-col items-center gap-4">
+      <UPopover
+        :content="{
+          align: 'center',
+          side: 'bottom',
+          sideOffset: 2,
+        }"
+      >
+        <UButton size="xl" icon="i-lucide-calendar" class="text-text-primary md:w-1/2 w-full" color="secondary">
+          {{ props.form.returnDate ? df.format(props.form.returnDate.toDate(getLocalTimeZone())) : 'Válassza ki a visszaút dátumát' }}
+        </UButton>
+        <template #content>
+          <UCalendar
+            :model-value="props.form.returnDate"
+            :year-controls="false"
+            size="lg"
+            :min-value="minDate"
+            @update:model-value="updateField('returnDate', $event)"
+            locale="hu"
+          />
+        </template>
+      </UPopover>
+
+      <UInput
+        v-maska="'##:##'"
+        :model-value="props.form.returnTime"
+        trailing-icon="i-lucide-timer"
+        label="Visszaút indulási ideje"
+        placeholder="Visszaút indulási ideje"
+        size="xl"
+        class="md:w-1/2 w-full"
+        @update:model-value="updateField('returnTime', $event)"
       />
     </div>
 
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -4,13 +4,10 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/fonts', '@gtm-support/nuxt-gtm'],
+  modules: ['@nuxt/ui', '@nuxt/fonts'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
-  },
-  gtm: {
-    id: 'GTM-WCLGKQ3P',
   },
   app: {
     head: {
