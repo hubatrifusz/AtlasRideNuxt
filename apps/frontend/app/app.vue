65ca238544401927e6d99625b9931d22ab7dcf6e
Huba Trifusz <huba.trifusz@gmail.com>
2025-11-24 20:48:46 +0100

    chore: remove Google Tag Manager (noscript) from app.vue

--- apps/frontend/app/app.vue
+++ apps/frontend/app/app.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- Google Tag Manager (noscript) -->
-  <noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCLGKQ3P" height="0" width="0" style="display: none; visibility: hidden"></iframe>
-  </noscript>
-  <!-- End Google Tag Manager (noscript) -->
   <UApp :locale="hu">
     <NuxtLayout>
       <NuxtRouteAnnouncer />
