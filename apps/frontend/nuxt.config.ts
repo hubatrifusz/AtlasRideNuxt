6cb37c5aa0e8c83864813fd66c23a860d97c4f2c
Huba Trifusz <huba.trifusz@gmail.com>
2025-11-24 16:41:24 +0100

    feat: add nuxt-gtag module for Google Analytics integration

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -6,8 +6,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/test-utils/module'],
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/test-utils/module', 'nuxt-gtag'],
   css: ['~/assets/css/main.css'],
+
+  gtag: {
+    id: 'G-JCVH01ZQ5W'
+  },
 
   vite: {
     plugins: [tailwindcss()],
